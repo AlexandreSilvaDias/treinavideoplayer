6f785600031a5e1768edb2ff6075f699c6a3589d
Alexandre <alexandre.silva.dias@outlook.com>
2023-10-06 14:37:23 -0300

    utilizacao do use Reducer

--- src/data/video/VideoContext.jsx
+++ src/data/video/VideoContext.jsx
@@ -1,44 +1,13 @@
-import React, {createContext} from "react";
+import React, {createContext, useReducer} from "react";
+import { videoInitialState, videoReducer } from "./VideoReducer";
 
-const _state = {
-    selectedVideo: {
-        id: 2,
-        title: 'Lions',
-        duration: 37,
-        url: 'https://v3.cdnpk.net/videvo_files/video/free/2019-11/large_watermarked/190301_1_25_11_preview.mp4',
-        cover: 'https://media.istockphoto.com/id/494856046/pt/foto/le%C3%A3o-em-alta-relva.jpg?s=1024x1024&w=is&k=20&c=-2MCGmaLNBpIUUv3iXBiVoKzXBupM6ZAra3UnEmyqig='
-    },
-    videos: [
-        {
-            id: 2,
-            title: 'Lions',
-            duration: 37,
-            url: 'https://v3.cdnpk.net/videvo_files/video/free/2019-11/large_watermarked/190301_1_25_11_preview.mp4',
-            cover: 'https://media.istockphoto.com/id/494856046/pt/foto/le%C3%A3o-em-alta-relva.jpg?s=1024x1024&w=is&k=20&c=-2MCGmaLNBpIUUv3iXBiVoKzXBupM6ZAra3UnEmyqig='
-        },
-        {
-            id: 5,
-            title: 'Lions',
-            duration: 37,
-            url: 'https://v3.cdnpk.net/videvo_files/video/free/2019-11/large_watermarked/190301_1_25_11_preview.mp4',
-            cover: 'https://media.istockphoto.com/id/494856046/pt/foto/le%C3%A3o-em-alta-relva.jpg?s=1024x1024&w=is&k=20&c=-2MCGmaLNBpIUUv3iXBiVoKzXBupM6ZAra3UnEmyqig='
-        },
-        {
-            id: 8,
-            title: 'Lions',
-            duration: 37,
-            url: 'https://v3.cdnpk.net/videvo_files/video/free/2019-11/large_watermarked/190301_1_25_11_preview.mp4',
-            cover: 'https://media.istockphoto.com/id/494856046/pt/foto/le%C3%A3o-em-alta-relva.jpg?s=1024x1024&w=is&k=20&c=-2MCGmaLNBpIUUv3iXBiVoKzXBupM6ZAra3UnEmyqig='
-        }
-    ]
-};
-
-export const videoStore = createContext(_state);
+export const videoStore = createContext(videoInitialState);
 const {Provider} = videoStore;
 
 export function VideoContext(props) {
+    const [state, dispatch] =useReducer(videoReducer, videoInitialState);
     return(
-        <Provider value={[_state]}>
+        <Provider value={[state, dispatch]}>
             {props.children}
         </Provider>
     )
